629d10c16370de020eccef714e982b17e0ece875
wretchedcoder <wretchedcoder@gmail.com>
2014-09-01 18:55:00 -0400

    Added GameEntity Object

---
+++ NobleQuest/NobleQuest/GameEntity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+
+namespace NobleQuest
+{
+    public class GameEntity
+    {
+        public Texture2D texture { get; set; }
+        public Vector2 position { get; set; }
+        public Vector2 velocity { get; set; }
+        public Vector2 midpoint { get; set; }
+        public float rotation { get; set; }
+        public Vector2 offset { get; set; }
+        public Rectangle rectangle { get; set; }
+        public Game game { get; set; }
+
+        public GameEntity() { }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+                this.texture,
+                this.position,
+                null,
+                Color.White,
+                this.rotation,
+                this.midpoint,
+                1.0f,
+                SpriteEffects.None,
+                1.0f);
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
---
+++ NobleQuest/NobleQuest/NobleQuestGame.cs
@@ -0,0 +1,105 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+#endregion
+
+namespace NobleQuest
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class NobleQuestGame : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        List<GameEntity> gameEntityList;
+
+        public NobleQuestGame()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            gameEntityList = new List<GameEntity>();
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            GameEntity gameEntity = new GameEntity();
+            gameEntity.texture = Content.Load<Texture2D>("City");
+            gameEntity.game = this;
+            gameEntity.midpoint = new Vector2(16.0f, 16.0f);
+            gameEntity.position = new Vector2(100f, 100f);
+
+            gameEntityList.Add(gameEntity);
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            this.spriteBatch.Begin();
+            for (int i = gameEntityList.Count - 1; i >= 0; i-- )
+            {
+                gameEntityList[i].Draw(this.spriteBatch);
+            }
+            this.spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
