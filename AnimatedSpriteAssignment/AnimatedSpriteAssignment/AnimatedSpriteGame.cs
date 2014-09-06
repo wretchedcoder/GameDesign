9221b218d877d76bfd2ff65c487f21b169003389
wretchedcoder <wretchedcoder@gmail.com>
2014-09-06 15:15:59 -0400

    Finished Assignment

--- AnimatedSpriteAssignment/AnimatedSpriteAssignment/AnimatedSpriteGame.cs
+++ AnimatedSpriteAssignment/AnimatedSpriteAssignment/AnimatedSpriteGame.cs
@@ -2,11 +2,12 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Storage;
-using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Media;
 #endregion
 
 namespace AnimatedSpriteAssignment
@@ -52,7 +53,7 @@ namespace AnimatedSpriteAssignment
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            spritefont = Content.Load<SpriteFont>("myFont");
+            spritefont = Content.Load<SpriteFont>("Arial");
         }
 
         /// <summary>
